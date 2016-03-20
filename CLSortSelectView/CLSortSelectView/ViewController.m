a19e995e3d2b7f68b8316f1b3aaa39153e8b9373
Darren-chenchen <1597887620@qq.com>
2016-03-20 17:19:14 +0800

    11

--- CLSortSelectView/CLSortSelectView/ViewController.m
+++ CLSortSelectView/CLSortSelectView/ViewController.m
@@ -24,7 +24,7 @@
     mainView.leftMenuSubViewHeight = 50;
     mainView.middleMenuSubViewHeight = 50;
     mainView.rightMenuSubViewHeight = 50;
-    mainView.leftMenuArrray = @[@"热门",@"经典",@"儿童专区",@"成人专区"];
+    mainView.leftMenuArrray = @[@"热门",@"经典"];
     mainView.middleMenuArray = @[@"定位",@"热门",@"上海",@"北京"];
     mainView.rightMenuArray = @[@"价格由低到高",@"价格由高到低",@"销量",@"好评"];
     mainView.frame = CGRectMake(0, 20, self.view.frame.size.width, 50);
